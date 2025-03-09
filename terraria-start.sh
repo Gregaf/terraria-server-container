3ba07a72563b5686ef8f07845e6b75340e37fc19
Greg Freitas <gregfreitas1997@gmail.com>
2025-03-09 18:17:51 -0400

    Updated terraria start script to create default world if none

--- terraria-start.sh
+++ terraria-start.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
+set -e
+
 worlds_directory="/home/terraria/worlds"
-
-max_worlds_to_load="$WORLD_MAX"
-terraria_server_bin="$TERRARIA_SERVER_BIN"
-
-if [ ! -d "$worlds_directory" ]; then
-    echo "Terraria worlds directory not found. Exiting..."
-    exit 1
-fi
+max_worlds_to_load="${WORLD_MAX:-1}"  # Default to 1 if WORLD_MAX is not set
+terraria_server_bin="${TERRARIA_SERVER_BIN:?TERRARIA_SERVER_BIN not set}"
 
 base_port=8000
 worlds_loaded=0
 
+mkdir -p "$worlds_directory"
+
+if ! compgen -G "$worlds_directory"/*.wld > /dev/null; then
+    echo "No worlds found. Creating a new large world..."
+    new_world_file="$worlds_directory/DefaultWorld.wld"
+
+    "$terraria_server_bin" -world "$new_world_file" -autocreate 3 -worldname "DefaultWorld" -seed "default" -noupnp -nosteam
+fi
+
 tmux new-session -d -s terraria-servers
 
 for world_file in "$worlds_directory"/*.wld; do
-    if [ -f "$world_file" ]; then
-        world_name=$(basename -- "$world_file" .wld)
-        
-        port=$((base_port++))
-        
-        tmux new-window -t terraria-servers -n "$world_name" "$terraria_server_bin -world \"$world_file\" -port $port"
-        
-        # Increment the counter
-        worlds_loaded=$((worlds_loaded + 1))
-        
-        if [ "$worlds_loaded" -ge "$max_worlds_to_load" ]; then
-            break
-        fi
-    fi
+    [[ -f "$world_file" ]] || continue
+
+    world_name=$(basename "$world_file" .wld)
+    port=$((base_port++))
+
+    tmux new-window -t terraria-servers -n "$world_name" \
+        "$terraria_server_bin -world \"$world_file\" -port $port"
+
+    ((worlds_loaded++))
+    [[ "$worlds_loaded" -ge "$max_worlds_to_load" ]] && break
 done
 
 tmux attach-session -t terraria-servers
